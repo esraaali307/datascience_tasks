893a0f596ce12c4b30145783121d94396b24c6b0
esraaali307 <52112744+esraaali307@users.noreply.github.com>
2019-06-23 20:43:52 +0200

    Add files via upload

---
+++ getphilosophy.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 13:12:26 2019
+
+@author: A
+"""
+
+import urllib2
+#import ssl
+
+#from bs4 import BeautifulSoup
+#import requests
+
+url = input ('enter url : ')
+no_links = int (input('enter position of philosophy')) #number of links between first link and philosophy 
+counter = 0
+def getphilo (normal_link):
+    if counter != position:
+    f = urllib2.urlopen(normal_link)
+    print(normal_link)
+    
